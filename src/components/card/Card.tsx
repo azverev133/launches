9881fa9fc6655a2684ca3fc07cfe3bb5d9bf7444
Artem Zverev <undemol133@gmail.com>
2021-12-05 15:28:54 +0400

    fix: img size

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -11,7 +11,7 @@ export const Card = (props: CardProps) => {
     console.log(props)
     return (
         <div className="card" style={{ width: props.width }}>
-            <img src={props.launch.links.mission_patch_small} alt={props.launch.mission_name} width={256} height={256}/>
+            <img src={props.launch.links.mission_patch_small} alt={props.launch.mission_name} width={128} height={128}/>
             <div className="info">
                 <div className="title-container">
                     <strong>{props.launch.mission_name}</strong>
